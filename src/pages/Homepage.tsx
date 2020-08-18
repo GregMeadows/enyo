f80b1272c49fa787f6a9dec249219a5b2e179326
Greg Meadows <git@gregmeadows.dev>
2020-08-18 18:03:43 +0100

    Initial files and site setup

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent } from 'react';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        margin: 0,
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: '14vw',
+        marginTop: '20vh',
+        marginBottom: 0,
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: '3vw',
+        marginTop: 0,
+        marginBottom: 0,
+    },
+}), {
+    classNamePrefix: 'homepage',
+});
+
+export const Homepage: FunctionComponent = () => {
+    const classes = useStyles();
+
+    return (
+        <section className={classes.root}>
+            <h1 className={classes.title}>enyo.gg</h1>
+            <h2 className={classes.subtitle}>Coming soon.</h2>
+        </section>
+    );
+};
