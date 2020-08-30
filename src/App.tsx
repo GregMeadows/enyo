178cd7d82e4cd1e5e1d150f3dd9fa0a0aad48226
Greg Meadows <git@gregmeadows.dev>
2020-08-30 16:18:49 +0100

    Add AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { Homepage } from './pages/Homepage';
+import ElevatedAppBar from './components/ElevatedAppBar';
 
 export const App: FunctionComponent = () => {
     return (
         <>
             <CssBaseline />
+            <ElevatedAppBar />
             <Homepage />
         </>
     );
---
+++ src/components/ElevatedAppBar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+interface ElevationScrollProps {
+    children: React.ReactElement;
+}
+
+function ElevationScroll(props: ElevationScrollProps) {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
+export default function ElevatedAppBar() {
+    return (
+        <>
+            <ElevationScroll>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6">ENYO</Typography>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <Toolbar />
+        </>
+    );
+}
