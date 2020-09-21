97c6bb5935e730fbf0ffd6c8bab2e241382c7be2
Greg Meadows <git@gregmeadows.dev>
2020-09-21 21:33:58 +0100

    Add logo heights and colour

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { Link, useLocation } from 'react-router-dom';
+import clsx from 'clsx';
 import { ReactComponent as LogoIconSvg } from '../images/logo/icon.svg';
 import { ReactComponent as LogoTextSvg } from '../images/logo/text.svg';
 import { ReactComponent as LogoFullSvg } from '../images/logo/full.svg';
@@ -11,14 +12,24 @@ interface LogoType {
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
-    root: {
-      height: theme.spacing(7),
+    svg: {
       transition: 'height 0.5s ease',
       userSelect: 'none',
     },
-    link: {
+    container: {
       padding: theme.spacing(0.5),
       display: 'inline-flex',
+    },
+    icon: {
+      height: theme.spacing(7),
+    },
+    text: {
+      height: '12vw',
+      minHeight: 160,
+    },
+    full: {
+      height: '8vw',
+      minHeight: 140,
     },
   }),
   {
@@ -39,26 +50,26 @@ const Logo: FunctionComponent<LogoType> = ({ type }) => {
   let logo;
   switch (type) {
     case 'text': {
-      logo = <LogoTextSvg className={classes.root} />;
+      logo = <LogoTextSvg className={clsx(classes.svg, classes.text)} />;
       break;
     }
     case 'full': {
-      logo = <LogoFullSvg className={classes.root} />;
+      logo = <LogoFullSvg className={clsx(classes.svg, classes.full)} />;
       break;
     }
     case 'icon':
     default: {
-      logo = <LogoIconSvg className={classes.root} />;
+      logo = <LogoIconSvg className={clsx(classes.svg, classes.icon)} />;
       break;
     }
   }
 
   if (isHomepage) {
-    return <div className={classes.link}>{logo}</div>;
+    return <div className={classes.container}>{logo}</div>;
   }
 
   return (
-    <Link to="/" className={classes.link}>
+    <Link to="/" className={classes.container}>
       {logo}
     </Link>
   );
