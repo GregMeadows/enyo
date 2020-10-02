9e4009adee73cda931d7a4acca51391e34597539
Greg Meadows <git@gregmeadows.dev>
2020-10-02 17:22:41 +0100

    Use flag for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,38 @@ import React, { FunctionComponent, Suspense } from 'react';
 import { observer } from 'mobx-react-lite';
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Footer from './components/Footer';
+import Layout from './components/Layout';
+import ElevatedAppBar from './components/ElevatedAppBar';
 import Loading from './components/Loading';
+import { getTheme } from './stores/settings';
 import ScrollToTop from './components/ScrollToTop';
 import Landing from './pages/landing';
 import themes from './themes';
 
+const DISPLAY_LANDING = true;
+
 const App: FunctionComponent = observer(() => {
   return (
-    <ThemeProvider theme={themes.default.dark}>
+    <ThemeProvider theme={DISPLAY_LANDING ? themes.default.dark : getTheme()}>
       <CssBaseline />
       <BrowserRouter>
         <ScrollToTop />
         <Suspense fallback={<Loading />}>
-          <Landing />
+          {DISPLAY_LANDING && <Landing />}
+          {!DISPLAY_LANDING && (
+            <>
+              <Layout>
+                <ElevatedAppBar />
+                <Switch>
+                  <Route path="/" exact component={Homepage} />
+                </Switch>
+              </Layout>
+              <Footer />
+            </>
+          )}
         </Suspense>
       </BrowserRouter>
     </ThemeProvider>
