0c4def89fd16063f68341c66e63c910527f9c281
Greg Meadows <git@gregmeadows.dev>
2020-11-22 15:41:46 +0000

    Add privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -11,36 +11,27 @@ import Loading from './components/Loading';
 import { getTheme } from './stores/settings';
 import ScrollToTop from './components/ScrollToTop';
 import PageTitle from './components/PageTitle';
-import Landing from './pages/landing';
-import themes from './themes';
 import Info from './pages/info';
 import { ROUTES_INFO, ROUTE_CONTACT } from './assets/routes';
 import Contact from './pages/Contact';
 
-const DISPLAY_LANDING = false;
-
 const App: FunctionComponent = observer(() => {
   return (
-    <ThemeProvider theme={DISPLAY_LANDING ? themes.default.dark : getTheme()}>
+    <ThemeProvider theme={getTheme()}>
       <CssBaseline />
       <BrowserRouter>
         <ScrollToTop />
         <Suspense fallback={<Loading />}>
-          {DISPLAY_LANDING && <Landing />}
-          {!DISPLAY_LANDING && (
-            <>
-              <Layout>
-                <ElevatedAppBar />
-                <PageTitle />
-                <Switch>
-                  <Route path="/" exact component={Homepage} />
-                  <Route path={ROUTES_INFO} exact component={Info} />
-                  <Route path={ROUTE_CONTACT} exact component={Contact} />
-                </Switch>
-              </Layout>
-              <Footer />
-            </>
-          )}
+          <Layout>
+            <ElevatedAppBar />
+            <PageTitle />
+            <Switch>
+              <Route path="/" exact component={Homepage} />
+              <Route path={ROUTES_INFO} exact component={Info} />
+              <Route path={ROUTE_CONTACT} exact component={Contact} />
+            </Switch>
+          </Layout>
+          <Footer />
         </Suspense>
       </BrowserRouter>
     </ThemeProvider>
--- src/pages/info/privacy.ts
+++ src/pages/info/privacy.ts
@@ -2,7 +2,68 @@ const privacy = {
   // t('pages.info.privacy.title')
   title: 'pages.info.privacy.title',
   updated: new Date(2020, 9, 26),
-  content: [],
+  content: [
+    // t('pages.info.privacy.main.1')
+    'pages.info.privacy.main.1',
+    // t('pages.info.privacy.collect.title')
+    'pages.info.privacy.collect.title',
+    // t('pages.info.privacy.collect.1')
+    'pages.info.privacy.collect.1',
+    // t('pages.info.privacy.collect.2')
+    'pages.info.privacy.collect.2',
+    // t('pages.info.privacy.collect.3')
+    'pages.info.privacy.collect.3',
+    // t('pages.info.privacy.collect.4')
+    'pages.info.privacy.collect.4',
+    // t('pages.info.privacy.collect.5')
+    'pages.info.privacy.collect.5',
+    // t('pages.info.privacy.use.title')
+    'pages.info.privacy.use.title',
+    // t('pages.info.privacy.use.1')
+    'pages.info.privacy.use.1',
+    // t('pages.info.privacy.use.2')
+    'pages.info.privacy.use.2',
+    // t('pages.info.privacy.use.3')
+    'pages.info.privacy.use.3',
+    // t('pages.info.privacy.share.title')
+    'pages.info.privacy.share.title',
+    // t('pages.info.privacy.share.1')
+    'pages.info.privacy.share.1',
+    // t('pages.info.privacy.share.2')
+    'pages.info.privacy.share.2',
+    // t('pages.info.privacy.share.3')
+    'pages.info.privacy.share.3',
+    // t('pages.info.privacy.ad.title')
+    'pages.info.privacy.ad.title',
+    // t('pages.info.privacy.ad.1')
+    'pages.info.privacy.ad.1',
+    // t('pages.info.privacy.ad.2')
+    'pages.info.privacy.ad.2',
+    // t('pages.info.privacy.ad.3')
+    'pages.info.privacy.ad.3',
+    // t('pages.info.privacy.dnt.title')
+    'pages.info.privacy.dnt.title',
+    // t('pages.info.privacy.dnt.1')
+    'pages.info.privacy.dnt.1',
+    // t('pages.info.privacy.rights.title')
+    'pages.info.privacy.rights.title',
+    // t('pages.info.privacy.rights.1')
+    'pages.info.privacy.rights.1',
+    // t('pages.info.privacy.rights.2')
+    'pages.info.privacy.rights.2',
+    // t('pages.info.privacy.retention.title')
+    'pages.info.privacy.retention.title',
+    // t('pages.info.privacy.retention.1')
+    'pages.info.privacy.retention.1',
+    // t('pages.info.privacy.changes.title')
+    'pages.info.privacy.changes.title',
+    // t('pages.info.privacy.changes.1')
+    'pages.info.privacy.changes.1',
+    // t('pages.info.privacy.contact.title')
+    'pages.info.privacy.contact.title',
+    // t('pages.info.privacy.contact.1')
+    'pages.info.privacy.contact.1',
+  ],
 };
 
 export default privacy;
