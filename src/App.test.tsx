16571835cadfe835183781be6222776df7731ca5
Greg Meadows <git@gregmeadows.dev>
2020-11-23 20:36:10 +0000

    Fix jest tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { I18nextProvider } from 'react-i18next';
 import App from './App';
+import i18n from './i18n';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
