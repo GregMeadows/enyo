d2f4a603f3500280d34ea9d4d612469a346c41ba
Greg Meadows <git@gregmeadows.dev>
2020-12-06 13:14:22 +0000

    Change homepage layout

---
+++ src/components/SVGPathOnView.tsx
@@ -0,0 +1,76 @@
+import React, { FunctionComponent, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { makeStyles, Theme } from '@material-ui/core';
+import { motion, useAnimation } from 'framer-motion';
+
+interface SVGPathOnViewType {
+  className?: string;
+}
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    path: {
+      fill: 'none',
+      stroke: theme.palette.primary.main,
+      strokeWidth: 3,
+    },
+    svg: {
+      height: 200,
+      width: '80vw',
+    },
+  }),
+  {
+    classNamePrefix: 'svg-path-on-view',
+  }
+);
+
+const SVGPathOnView: FunctionComponent<SVGPathOnViewType> = ({ className }) => {
+  const classes = useStyles();
+  const animation = useAnimation();
+  const [viewRef, inView] = useInView({
+    threshold: 0.6,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      animation.start('visible');
+    }
+  }, [animation, inView]);
+
+  const pathVariants = {
+    hidden: {
+      pathLength: 0,
+      opacity: 0,
+    },
+    visible: {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        delay: 0.6,
+        duration: 1.2,
+        ease: 'easeInOut',
+      },
+    },
+  };
+
+  return (
+    <div ref={viewRef} className={className}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 500 200"
+        preserveAspectRatio="none"
+        className={classes.svg}
+      >
+        <motion.path
+          d="M2 2 V 196 H 496"
+          className={classes.path}
+          initial="hidden"
+          animate={animation}
+          variants={pathVariants}
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default SVGPathOnView;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,18 +1,15 @@
 import React, { FunctionComponent } from 'react';
 import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import { Trans, useTranslation } from 'react-i18next';
-import { Typography, useMediaQuery } from '@material-ui/core';
-import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
+import { Grid, Typography, useMediaQuery } from '@material-ui/core';
 import ImageScroller from '../components/ImageScroller';
 import bannerImg from '../images/banner.png';
 import useTitle from '../hooks/useTitle';
 import enyoProHomeFrontImg from '../images/clothing/enyo/pro/home.front.png';
-import enyoProHomeBackImg from '../images/clothing/enyo/pro/home.back.png';
 import enyoProAwayFrontImg from '../images/clothing/enyo/pro/away.front.png';
-import enyoProAwayBackImg from '../images/clothing/enyo/pro/away.back.png';
 import bannerTextImg from '../images/banner.text.png';
 import { isLightMode } from '../stores/settings';
-import { BREAKPOINT_MOBILE } from '../assets/consts';
+import { BREAKPOINT_MOBILE, BREAKPOINT_TABLET } from '../assets/consts';
 import WingedBorder from '../components/WingedBorder';
 import EnterOnView from '../components/EnterOnView';
 
@@ -36,74 +33,35 @@ const useStyles = makeStyles(
       width: '90%',
       maxWidth: '45rem',
     },
-    tee: {
-      width: '16rem',
-    },
-    kit: {
-      margin: '5rem 0',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'relative',
-    },
     rotated: {
       display: 'inline-flex',
       alignItems: 'baseline',
       textTransform: 'uppercase',
-      transform: 'rotate(90deg) translate(50%, 0)',
+      transform: 'rotate(-90deg) translate(-50%, 20%)',
+      transformOrigin: '0 0',
       position: 'absolute',
-      '& > svg': {
-        marginLeft: '0.4rem',
-        height: '1em',
-        width: '1em',
-        fontSize: '0.7rem',
-      },
-    },
-    rotatedRight: {
-      right: 0,
-    },
-    rotatedLeft: {
       left: 0,
     },
-    float: {
-      position: 'absolute',
-      width: '28rem',
-      '& > h6': {
-        textTransform: 'uppercase',
+    rotatedContainer: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    title: {
+      margin: '2rem 4vw',
+    },
+    kitItem: {
+      padding: '4vw',
+      textAlign: 'center',
+    },
+    kitInfo: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      padding: '0 9vw',
+      [theme.breakpoints.down(BREAKPOINT_TABLET)]: {
+        order: 1,
+        padding: '2vw 6vw',
       },
-    },
-    floatDesign: {
-      top: '3%',
-      right: '16%',
-    },
-    floatFit: {
-      top: '45%',
-      left: '15%',
-      textAlign: 'right',
-    },
-    floatFabric: {
-      bottom: '5%',
-      right: '15%',
-    },
-    floatBody: {
-      marginTop: theme.spacing(1),
-    },
-    line: {
-      position: 'absolute',
-      strokeWidth: 2,
-      stroke: theme.palette.primary.main,
-    },
-    lineDesign: {
-      left: -125,
-    },
-    lineFit: {
-      left: 150,
-      top: -75,
-    },
-    lineFabric: {
-      left: -125,
-      top: -35,
     },
   }),
   {
@@ -132,92 +90,35 @@ const Homepage: FunctionComponent = () => {
         <img src={bannerTextImg} alt={t('company.tag')} />
       </ImageScroller>
       <section className={classes.info}>
-        <WingedBorder left right direction="down" length={90} />
-        <div className={clsx(classes.rotated, classes.rotatedLeft)}>
-          <Typography variant="subtitle1">
-            <Trans i18nKey="pages.homepage.decor.loading" />
-          </Typography>
-          <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-            <rect width="10" height="10" fill="currentColor" />
-          </svg>
-        </div>
-        <EnterOnView className={classes.kit}>
-          <img
-            src={isLightMode() ? enyoProHomeFrontImg : enyoProAwayFrontImg}
-            alt={t('pages.homepage.kit.front')}
-          />
-          <div className={clsx(classes.float, classes.floatDesign)}>
-            <Typography variant="h6">
-              {t('pages.homepage.kit.design.title')}
+        <Typography variant="h1" className={classes.title}>
+          {t('pages.homepage.kit.title')}
+        </Typography>
+        <WingedBorder right direction="down" length={85} />
+        <Grid container>
+          <Grid item className={classes.rotatedContainer}>
+            <div className={classes.rotated}>
+              <Typography variant="subtitle1">
+                {t('pages.homepage.kit.rotated')}
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6} className={classes.kitInfo}>
+            <Typography variant="body1" paragraph>
+              {t('pages.homepage.kit.text.1')}
             </Typography>
-            <svg
-              height={60}
-              width={420}
-              className={clsx(classes.line, classes.lineDesign)}
-            >
-              <line x1={0} x2={120} y1={60} y2={2} />
-              <line x1={120} x2={420} y1={2} y2={2} />
-            </svg>
-            <Typography variant="body2" className={classes.floatBody}>
-              {t('pages.homepage.kit.design.1')}
+            <Typography variant="body1">
+              {t('pages.homepage.kit.text.2')}
             </Typography>
-          </div>
-        </EnterOnView>
-        <WingedBorder right direction="up" length={80} />
-        <EnterOnView className={classes.kit}>
-          <img
-            src={isLightMode() ? enyoProHomeBackImg : enyoProAwayBackImg}
-            alt={t('pages.homepage.kit.back')}
-          />
-          <div className={clsx(classes.float, classes.floatFit)}>
-            <Typography variant="h6">
-              {t('pages.homepage.kit.fit.title')}
-            </Typography>
-            <svg
-              height={102}
-              width={420}
-              className={clsx(classes.line, classes.lineFit)}
-            >
-              <line x1={0} x2={300} y1={100} y2={100} />
-              <line x1={300} x2={420} y1={100} y2={2} />
-            </svg>
-            <Typography variant="body2" className={classes.floatBody}>
-              {t('pages.homepage.kit.fit.1')}
-            </Typography>
-          </div>
-        </EnterOnView>
-        <WingedBorder left direction="down" length={80} />
-        <div className={clsx(classes.rotated, classes.rotatedRight)}>
-          <Typography variant="subtitle1">
-            <Trans i18nKey="pages.homepage.decor.loading2" />
-          </Typography>
-          <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-            <rect width="10" height="10" fill="currentColor" />
-          </svg>
-        </div>
-        <EnterOnView className={classes.kit}>
-          <img
-            src={isLightMode() ? enyoProHomeFrontImg : enyoProAwayFrontImg}
-            alt={t('pages.homepage.kit.front')}
-          />
-          <div className={clsx(classes.float, classes.floatFabric)}>
-            <Typography variant="h6">
-              {t('pages.homepage.kit.fabric.title')}
-            </Typography>
-            <svg
-              height={62}
-              width={420}
-              className={clsx(classes.line, classes.lineFabric)}
-            >
-              <line x1={0} x2={120} y1={0} y2={60} />
-              <line x1={120} x2={420} y1={60} y2={60} />
-            </svg>
-            <Typography variant="body2" className={classes.floatBody}>
-              {t('pages.homepage.kit.fabric.1')}
-            </Typography>
-          </div>
-        </EnterOnView>
-        <WingedBorder left right direction="up" length={90} />
+          </Grid>
+          <Grid item xs={12} md={6} className={classes.kitItem}>
+            <EnterOnView>
+              <img
+                src={isLightMode() ? enyoProHomeFrontImg : enyoProAwayFrontImg}
+                alt={t('pages.homepage.kit.front')}
+              />
+            </EnterOnView>
+          </Grid>
+        </Grid>
       </section>
     </>
   );
