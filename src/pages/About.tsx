27235e5359a0c2cb252b18779f1135098d15e8e0
Greg Meadows <git@gregmeadows.dev>
2021-01-11 15:12:06 +0000

    Add enyo image to about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,31 @@
 import React, { FunctionComponent } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles, Theme } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
-import Main from '../components/Main';
+import enyoColor from '../images/enyo.color.png';
+import WingedBorder from '../components/WingedBorder';
 
 const useStyles = makeStyles(
-  () => ({
+  (theme: Theme) => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      position: 'relative',
+    },
+    text: {
+      margin: '2rem 40vw 12rem 4vw',
+      background: fade(theme.palette.background.default, 0.6),
+    },
     title: {
       marginTop: '2rem',
       marginBottom: '2rem',
+    },
+    imageContainer: {
+      position: 'absolute',
+      zIndex: -1,
+      bottom: 3,
+      right: -100,
+      width: '60vw',
     },
   }),
   {
@@ -21,20 +38,26 @@ const About: FunctionComponent = () => {
   const { t } = useTranslation();
 
   return (
-    <Main>
-      <Typography variant="h1" className={classes.title}>
-        {t('pages.about.title')}
-      </Typography>
-      <Typography variant="body1" paragraph>
-        {t('pages.about.1')}
-      </Typography>
-      <Typography variant="body1" paragraph>
-        {t('pages.about.2')}
-      </Typography>
-      <Typography variant="body1" paragraph>
-        {t('pages.about.3')}
-      </Typography>
-    </Main>
+    <div className={classes.root}>
+      <div className={classes.text}>
+        <Typography variant="h1" className={classes.title}>
+          {t('pages.about.title')}
+        </Typography>
+        <Typography variant="body1" paragraph>
+          {t('pages.about.1')}
+        </Typography>
+        <Typography variant="body1" paragraph>
+          {t('pages.about.2')}
+        </Typography>
+        <Typography variant="body1" paragraph>
+          {t('pages.about.3')}
+        </Typography>
+      </div>
+      <div className={classes.imageContainer}>
+        <img src={enyoColor} alt={t('company.name')} />
+      </div>
+      <WingedBorder left direction="down" length={70} />
+    </div>
   );
 };
 export default About;
