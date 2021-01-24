856158c5acdff476960d7b6bccbebbf55a619d9c
Greg Meadows <git@gregmeadows.dev>
2021-01-24 12:26:18 +0000

    Improve StyledButton responsivness

--- src/components/StyledButton.tsx
+++ src/components/StyledButton.tsx
@@ -2,6 +2,11 @@ import React, { FunctionComponent, useEffect, useState } from 'react';
 import { makeStyles, Theme, withStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import { motion, useAnimation } from 'framer-motion';
+import {
+  BREAKPOINT_LAPTOP,
+  BREAKPOINT_MOBILE,
+  BREAKPOINT_TABLET,
+} from '../assets/consts';
 
 const StyledMuiButton = withStyles((theme: Theme) => ({
   root: {
@@ -10,6 +15,18 @@ const StyledMuiButton = withStyles((theme: Theme) => ({
     clipPath: 'polygon(0 0, 84% 0, 100% 30%, 100% 100%, 0 100%)',
     padding: '0.6rem 3.2rem',
     fontSize: '1.1rem',
+    [theme.breakpoints.down(BREAKPOINT_LAPTOP)]: {
+      fontSize: '1rem',
+      padding: '0.6rem 3rem',
+    },
+    [theme.breakpoints.down(BREAKPOINT_TABLET)]: {
+      fontSize: '1rem',
+      padding: '0.6rem 2.8rem',
+    },
+    [theme.breakpoints.down(BREAKPOINT_MOBILE)]: {
+      fontSize: '0.9rem',
+      padding: '0.5rem 2.5rem',
+    },
   },
   label: {
     pointerEvents: 'none',
