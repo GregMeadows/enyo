3a69b6aa5cd1ff6d8eb76b4884ef517d69de7243
Greg Meadows <git@gregmeadows.dev>
2021-02-09 20:15:14 +0000

    Remove action page

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import {
   ROUTES_INFO,
   ROUTE_ABOUT,
   ROUTE_ACCOUNT,
-  ROUTE_ACTION,
   ROUTE_CONTACT,
   ROUTE_STORE,
 } from './assets/routes';
@@ -24,7 +23,6 @@ import Contact from './pages/Contact';
 import About from './pages/About';
 import Store from './pages/Store';
 import Account from './pages/Account';
-import Action from './pages/Action';
 
 const App: FunctionComponent = observer(() => (
   <ThemeProvider theme={getTheme()}>
@@ -44,7 +42,6 @@ const App: FunctionComponent = observer(() => (
                 <Route path={ROUTE_CONTACT} exact component={Contact} />
                 <Route path={ROUTE_STORE} exact component={Store} />
                 <Route path={ROUTE_ACCOUNT} exact component={Account} />
-                <Route path={ROUTE_ACTION} exact component={Action} />
               </Switch>
             </>
           </Switch>
